f64d932e214137e7d533a0f311b7b6c3a034d9f1
Tiago Costa <tiagodpv@gmail.com>
2022-11-05 16:34:08 -0400

    routes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold}  from '@expo-google-fonts/roboto';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'; 
@@ -20,7 +20,7 @@ export default function App() {
         translucent
       />
       
-      {  fontsLoaded ? <SignIn /> : <Loading/> }
+      {  fontsLoaded ? <Routes /> : <Loading/> }
       </AuthContextProvider>      
     </NativeBaseProvider>
   );
